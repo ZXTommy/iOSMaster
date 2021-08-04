01e0b66aa4d9b769230daabcc1e269e9dbc0ad71
zhaoxin <zhaoxin7649@163.com>
2021-08-04 16:46:24 +0800

    第四章提交

---
+++ chapter4/class_isa/class_isa/ZXPerson.h
@@ -0,0 +1,19 @@
+//
+//  ZXPerson.h
+//  isa_test
+//
+//  Created by zx on 2021/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZXPerson : NSObject
+{
+    NSString *name;
+}
+@property (nonatomic,strong) NSString *nikeName;
+@end
+
+NS_ASSUME_NONNULL_END
